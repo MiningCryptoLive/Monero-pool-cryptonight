bfa1f6e56aa7ea230b56fcb8044de9f563d73f9f
MiningCryptoLive <celldr69@gmail.com>
2024-11-12 08:42:48 -0500

    Update config.js

--- website_example/config.js
+++ website_example/config.js
@@ -1,17 +1,17 @@
-var api = "https://multi-miner.smartcoinpool.net:8119";
-let parentCoin = "Arqma"
+var api = "https://xmr.solopool.us";
+var poolHost = "xmr.solopool.us";
+let parentCoin = "XMR"
 
-var email = "support@poolhost.com";
+var email = "support@miningcryptolive@gmail.com";
 var telegram = "https://t.me/YourPool";
 var discord = "https://discordapp.com/invite/YourPool";
-var facebook = "https://www.facebook.com/<YourPoolFacebook>";
+
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
-var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
+var blockchainExplorer = "https://blockchair.com/{symbol}/block/{id}";
+var transactionExplorer = "https://blockchair.com/{symbol}/transaction/{id}";
+
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
